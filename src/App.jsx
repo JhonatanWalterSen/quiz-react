e1dd18a205f2f65a4fbaef76847210202c49d6a7
Jhonatan Ariza <arizaj@LIM915010.local>
2022-12-21 03:02:20 -0500

    actualizar preguntas

---
+++ src/App.jsx
@@ -0,0 +1,123 @@
+import { useState, useEffect } from 'react'
+import preguntas from './helpers/preguntas'
+
+
+function App() {
+  const [preguntaActual, setPreguntaActual] = useState(0)
+  const [puntuacion, setPuntuacion] = useState(0)
+  const [isFinished, setIsFinished] = useState(false)
+  const [tiemporestante, setTiempoRestante] = useState(10)
+  const [areDisabled, setAreDisabled] = useState(false)
+  const [answersShow, setAnswersShow] = useState(false)
+
+  function handleAnswerSubmit(isCorrect, e) {
+    if (isCorrect) setPuntuacion(puntuacion + 1)
+    e.target.classList.add(isCorrect? "correct": "incorrect")
+    setTimeout(() => {
+      if (preguntaActual === preguntas.length - 1) {
+        setIsFinished(true)
+      } else{
+        setPreguntaActual(preguntaActual+1)
+      }
+    }, 400);
+  }
+
+  useEffect(()=>{
+    const interval = setInterval(() => {
+      if (tiemporestante > 0) setTiempoRestante(tiemporestante - 1)
+      if (tiemporestante === 0) setAreDisabled(true)
+    }, 1000);
+    return() => clearInterval(interval)
+  },[tiemporestante])
+
+  if (isFinished)
+  return (
+    <main className='app'>
+      <div className='juego-terminado'>
+        <span>
+          {" "}
+          Obtuviste {puntuacion}  de {preguntas.length}{" "}
+        </span>
+        <button  onClick={() =>{ window.location.href ="/"}}>
+          {" "}
+          Volver a jugar
+        </button>
+        <button  onClick={() =>{
+          setIsFinished(false)
+          setAnswersShow(true)
+          setPreguntaActual(0)
+        }}>
+          Ver Respuestas
+        </button>
+      </div>
+    </main>
+  )
+  if (answersShow) return <main className='app'>
+
+  <div className='lado-izquierdo'>
+        <div className='numero-pregunta'>
+          <span>Pregunta {preguntaActual+1} de</span> {preguntas.length}
+        </div>
+        <div className="titulo-pregunta">
+          {preguntas[preguntaActual].titulo} {" "}
+        </div>
+        <div>
+          {
+            preguntas[preguntaActual].opciones.filter(
+              (opcion) => opcion.isCorrect
+            )[0].textoRespuesta
+          }
+        </div>
+        <button onClick={()=>{
+          if (preguntaActual === preguntas.length - 1) {
+            window.location.href='/'
+          } else{
+            setPreguntaActual(preguntaActual+1)
+          }
+        }}>
+          {preguntaActual === preguntas.length -1
+          ? "Volver a jugar"
+          : 'Siguiente'}
+        </button>
+      </div>
+  </main>
+
+  return (
+    <main className='app'>
+
+      <div className='lado-izquierdo'>
+        <div className='numero-pregunta'>
+          <span>Pregunta {preguntaActual+1} de</span> {preguntas.length}
+        </div>
+        <div className="titulo-pregunta">
+          {preguntas[preguntaActual].titulo} {" "}
+        </div>
+        <div>
+        {!areDisabled ? (
+          <span className='tiempo-restante'>Tiempo restante: {tiemporestante}</span>
+        ):(
+            <button onClick={() =>{
+              setTiempoRestante(10)
+              setAreDisabled(false)
+              setPreguntaActual(preguntaActual+1)
+            }}>Continuar</button>
+          )
+        }
+        </div>
+      </div>
+
+      <div className='lado-derecho'>
+        {preguntas[preguntaActual].opciones.map(opcion =>(
+          <button
+            disabled={areDisabled}
+            key={opcion.textoRespuesta}
+            onClick={(e) => handleAnswerSubmit(opcion.isCorrect, e)}
+          >{opcion.textoRespuesta}</button>
+        ))}
+      </div>
+
+    </main>
+  )
+}
+
+export default App
---
+++ src/helpers/preguntas.js
@@ -0,0 +1,31 @@
+const preguntas = [
+    {
+        titulo: "¿Cual es el mejor lenguaje de programación?",
+        opciones: [
+            {textoRespuesta: 'Javascript', isCorrect: true},
+            {textoRespuesta: 'PHP', isCorrect: false},
+            {textoRespuesta: 'C++', isCorrect: false},
+            {textoRespuesta: 'Kotlin', isCorrect: false},
+        ]
+    },
+    {
+        titulo: "¿Respuesta C?",
+        opciones: [
+            {textoRespuesta: 'A', isCorrect: false},
+            {textoRespuesta: 'B', isCorrect: false},
+            {textoRespuesta: 'C', isCorrect: true},
+            {textoRespuesta: 'F', isCorrect: false},
+        ]
+    },
+    {
+        titulo: "¿Cuanto es '11' + 1 en javascript?",
+        opciones: [
+            {textoRespuesta: '111', isCorrect: true},
+            {textoRespuesta: '12', isCorrect: false},
+            {textoRespuesta: 'Syntax Error', isCorrect: false},
+            {textoRespuesta: '11', isCorrect: false},
+        ]
+    },
+]
+
+export default preguntas
